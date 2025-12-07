61bbf76cc91374a4cbf3d5ed84c7278def710dd9
bolachong <pchong@pioix.edu.ar>
2025-12-07 20:49:16 -0300

    f
    f

--- practica2/src/app/turnos/page.js
+++ practica2/src/app/turnos/page.js
@@ -1,13 +1,22 @@
 "use client"
 
 import TurnosC from "@/components/Turnos"
+import { useSocket } from "@/hooks/useSocket"
+import { useEffect } from "react";
 
 export default function Turnos() {
+
+    const {socket, isConnected} = useSocket();
+
+
+
+    socket.on("pingAll", (data) => {
+        console.log(data);
+    });
+
     return(
         <>
-            <TurnosC
-                especialidad="Terapeuta"
-            ></TurnosC>
+            <h1>Socket funcionando</h1>
         </>
     )
 }
